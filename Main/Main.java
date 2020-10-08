1217bd4ef2e808bac936fe1ab703d5f57bfe9bdf
yeomyeom <ysy9639@naver.com>
2020-10-08 15:36:48 +0900

    새로운 text 파일 추가

--- Main/Main.java
+++ Main/Main.java
@@ -1,72 +1,60 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class Main {
-
 	public static void main(String[] args) {
-		
+		String filename;
 		try {
 			String option = args[0];
-			String filename;
 			if(option.equals("-v")) {
 				filename = args[1];
-				// open txt file
-				System.out.println("-v option selected");
-			}
-			else {
-				filename = option;
-				try {
-					// open txt file
-					File f = new File(filename);
-					FileReader f_read = new FileReader(f);
-					BufferedReader buf = new BufferedReader(f_read);
-					String l;
-					ArrayList<String> words = new ArrayList<String>();
-					ArrayList<String> words_token = new ArrayList<String>();
-					while((l = buf.readLine()) != null) {
-						words = new ArrayList<String>();
-						words_token = new ArrayList<String>();
-						words = splitWord(l);
-						//for TEST print line by line
-						System.out.println("\n===WORDS===");
-						for(int i=0; i<words.size(); i++) {
-							System.out.print(words.get(i) + " ");
-						}
-						//for TEST print token
-						System.out.println("\n===TOKEN===");
-						words_token = tokenize(words);
-						for(int i=0; i<words_token.size(); i++) {
-							System.out.print(words_token.get(i) + " ");
-						}
-						//qualify statement has no error or warning
-						
-						//일단 정상 범주까지만 해놓고 warning 하자
-					}
-					buf.close();
-				}catch(FileNotFoundException e) {
-					System.out.println("file");
-				}catch(IOException e) {
-					System.out.println("text file encoding wrong");
+				// v 옵션이 눌렸을때 토큰들 주르륵 출력
+				for(String token : lexical(filename)) {
+					System.out.println(token);
 				}
-				
+			}else {
+				filename = option;
+				// 문법에 맞는지 체크하고 simbol 테이블 만들기
+				ArrayList<String> token_list = lexical(filename);
 			}
-			
 		}catch(java.lang.ArrayIndexOutOfBoundsException e){
-			System.out.println("File name is null");
+			// java -jar Main.jar 이후에 인자 잘못 입력하면 에러메시지 출력
+			System.out.println("args Error");
 		}
 	}
-	
-	public static ArrayList<String> splitWord(String input) {
-		ArrayList<String> words = new ArrayList<String>();
-		for(String word : input.split(" ")) {
-			words.add(word);
+	public static ArrayList<String> lexical(String file_name){
+		try {
+			// open txt file
+			File f = new File(file_name);
+			Scanner s = new Scanner(f);
+			ArrayList<String> code = new ArrayList<String>();// 코드 한줄한줄
+			ArrayList<String> word = new ArrayList<String>();// 코드 띄어쓰기 기준
+			ArrayList<String> token_string = new ArrayList<String>();// 코드를 토큰화 한것
+			while(s.hasNextLine()) {
+				code.add(s.nextLine());
+			}
+			for (String line : code) {
+				String[] lines = line.split(" ");
+				for (String words : lines) {
+					word.add(words);
+				}
+			}
+			token_string = tokenize(word);
+			s.close();
+			return token_string;
+		}catch(FileNotFoundException e) {
+			System.out.println("file");
+			return null;
+		}catch(IOException e) {
+			System.out.println("text file encoding wrong");
+			return null;
 		}
-		return words;
 	}
 	public static ArrayList<String> tokenize(ArrayList<String> input) {
 		ArrayList<String> patterns = new ArrayList<String>();
@@ -75,8 +63,8 @@ public class Main {
 		patterns.add("^[a-zA-Z_][0-9a-zA-Z_]*$"); //IDNET
 		patterns.add(":="); //ASSIGNMENT_OP
 		patterns.add(";"); //SEMI_COLON
-		patterns.add("\\+ | \\-"); // ADD_OPERATOR
-		patterns.add("\\* | /"); // MULT_OPERATOR
+		patterns.add("\\+|\\-"); // ADD_OPERATOR
+		patterns.add("\\*|/"); // MULT_OPERATOR
 		patterns.add("\\("); // LEFT_PATEN
 		patterns.add("\\)"); // RIGHT_PATEN
 		for(int i = 0 ; i<input.size() ; i++) {
@@ -85,7 +73,7 @@ public class Main {
 					if(p==0) {
 						output.add("CONST");
 					}else if(p==1) {
-						output.add("IDNET");
+						output.add("IDENT");
 					}else if(p==2) {
 						output.add("ASSIGNMENT_OP");
 					}else if(p==3) {
@@ -106,6 +94,4 @@ public class Main {
 		}
 		return output;
 	}
-	
-	//simbol table 생성 해야함
 }
